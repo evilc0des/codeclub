eb92e58d4681fc396c8733654c8165c2969fa36b
dkRaptor <dhrtklpsh82@gmail.com>
2015-11-08 19:03:19 +0530

    Login page ver0.2

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    $(".login-submit").hover(function(){
+		$(this).css({"background-color":"#A43537", "color":"white"});
+		},function(){
+			$(this).css({"background-color":"#FFD708", "color":"black"});
+			});
+});
